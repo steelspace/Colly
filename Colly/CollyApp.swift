3c919da323b592191499987ed51b24f423974bed
Petr Valach <petr.valach@hotmail.com>
2024-02-02 10:16:24 +0100

    Initial Commit

---
+++ Colly/CollyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollyApp.swift
+//  Colly
+//
+//  Created by Petr Valach on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CollyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
