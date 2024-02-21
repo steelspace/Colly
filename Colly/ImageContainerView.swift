b7bb67af272a7db11fcea0d40da39a4bbfaeae49
Petr Valach <petr.valach@hotmail.com>
2024-02-21 21:40:51 +0100

    a

---
+++ Colly/ImageContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageContainerView.swift
+//  Colly
+//
+//  Created by Petr Valach on 20.02.2024.
+//
+
+import SwiftUI
+
+struct ImageContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageContainerView()
+}
